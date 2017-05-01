4f966b6769c8104aea2bc96719a583ccc7d7fd89
Daniel Bischof <daniel.bischof90@gmail.com>
2017-05-01 18:22:26 +0200

    Changed to two large mixed portfolios for reference test, included more informative output, added more layers of security for analytical recalibration.

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ from models.common.structs import Profile
 from multiprocessing import Pool
 
 profile1 = Profile()
-profile1.SAMPLESIZE = 1000000
-profile1.NAME = "Full, standard"
-profile1.RES = 150
+profile1.SAMPLESIZE = 6000000
+profile1.NAME = "Ten assets, full"
+profile1.RES = 450
 profile1.RISKMEASURES = ["ES", "VaR"]
 profile1.SET = "full"
-profile1.BURNIN = 50
+profile1.BURNIN = 100
+profile1.SCALES = {'first': [1,1,1,1,1,1,1,1,1,1], 'second': [1.3,0.8,1,0.6, 1.1, 1.9, 0.9, 1.1, 1.4, 1.5]}
 
 profile2 = Profile()
 profile2.SAMPLESIZE = 6000000
-profile2.NAME = "Ten assets"
-profile2.RES = 500
+profile2.NAME = "Ten assets, mixed"
+profile2.RES = 450
 profile2.RISKMEASURES = ["ES", "VaR"]
-profile2.SET = "full"
-profile2.BURNIN = 50
+profile2.SET = "mixed"
+profile2.BURNIN = 100
 profile2.SCALES = {'first': [1,1,1,1,1,1,1,1,1,1], 'second': [1.3,0.8,1,0.6, 1.1, 1.9, 0.9, 1.1, 1.4, 1.5]}
 
 
--- scripts/runprofile.py
+++ scripts/runprofile.py
@@ -26,7 +26,7 @@ def giveInfos(listOfProfiles, numWorkers):
           '\n' + numProcesses + '\n')
 
 def runProfile(profile):
-    print("Profile \'" + profile.NAME + "\' launched.")
+    print("Profile \'" + profile.NAME + "\' launched at " + str((datetime.now()).replace(microsecond=0))[11:] + ".")
     startTotal = time()
     infoStruct = HiddenRegularInformation(profile)
     paraStruct = ParameterForPortfolio(profile)
@@ -44,7 +44,9 @@ def runProfile(profile):
         empiricalPortfolio.goodGuess["cubeSize"] = 0.15
         analyticalOptimalPortfolios = []
         empiricalOptimalPortfolios = []
-        recalCount = 0
+        recalCountAna = []
+        recalCountEmp = []
+        noPortfolio = []
         results[measure] = {}
         profile.RESULTINFORMATION[measure] = {}
         for step in range(profile.RES + profile.BURNIN):
@@ -56,7 +58,20 @@ def runProfile(profile):
                 surrogateStart = empiricalPortfolio.optimalPortfolio.fun * (analyticalPortfolio.para.IND_REG - 1)/analyticalPortfolio.para.IND_REG
                 analyticalPortfolio.updateOptimizationConstraints(analyticalOptimalPortfolios[-1].x)
                 analyticalPortfolio.getOptimalPortfolio(measure, level, surrogateStart)
-                recalCount += 1
+                recalCountAna.append(level)
+                if analyticalPortfolio.optimalPortfolio.fun < 0 or \
+                        (len(analyticalOptimalPortfolios) > 0 and analyticalPortfolio.optimalPortfolio.fun < analyticalOptimalPortfolios[-1].fun):
+                    print("Warning: Process \'" + profile.NAME + "\' needs empirical approximation of start value at level " + str(round(level, 2)) +
+                    ". Calculate empirical surrogate approximation and restart step.")
+                    recalCountEmp.append(level)
+                    surrogateStart = empiricalPortfolio.getOptimalPortfolio("VaR", level, "mixed", getOutput=True)
+                    analyticalPortfolio.updateOptimizationConstraints(surrogateStart.x)
+                    analyticalPortfolio.getOptimalPortfolio(measure, level, surrogateStart.fun)
+                    if analyticalPortfolio.optimalPortfolio.fun < 0 or \
+                            (len(analyticalOptimalPortfolios) > 0 and analyticalPortfolio.optimalPortfolio.fun < analyticalOptimalPortfolios[-1].fun):
+                        print("Warning: Process \'" + profile.NAME + "\' failed to find optimal value at level " + str(round(level, 2)) + ".")
+                        noPortfolio.append(level)
+
             if step > profile.BURNIN:
                 analyticalOptimalPortfolios.append(analyticalPortfolio.optimalPortfolio)
                 empiricalOptimalPortfolios.append(empiricalPortfolio.optimalPortfolio)
@@ -65,9 +80,12 @@ def runProfile(profile):
                 print("Progress notification: Process \'" + profile.NAME + "\' has completed roughly " +
                       str(round(step * 5/(profile.RES + profile.BURNIN))) + "/5 of its steps for " + measure +". Estimated end time: " +
                       str((datetime.now() + timedelta(seconds=remainingSeconds)).replace(microsecond=0))[11:])
+
         results[measure]["analytical"] = analyticalOptimalPortfolios
         results[measure]["empirical"] = empiricalOptimalPortfolios
-        profile.RESULTINFORMATION[measure]["analyticalRecal"] = recalCount
+        profile.RESULTINFORMATION[measure]["Analytical Recalibration"] = recalCountAna
+        profile.RESULTINFORMATION[measure]["Empirical Recalibration"] = recalCountEmp
+        profile.RESULTINFORMATION[measure]["No Portfolio"] = noPortfolio
 
     print("Profile \'" + profile.NAME + "\' completed. Total time: " + str(round(time() - startTotal, 2)) + " seconds.")
     return dict(data=results, profile=profile)
